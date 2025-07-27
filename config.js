3a758beb555c2d3cb729880ec818d3d86501bc45
haxwithaxe <spam@haxwithaxe.net>
2025-07-27 13:15:04 -0400

    Added test video tile to default config.

--- config.js
+++ config.js
@@ -129,8 +129,8 @@ const userSettings = {
     },
     {
       title: "",
-      sources: "https://www.hamqsl.com/solarsmall.php",
-      refreshInterval: "1h",
+      sources: "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4",
+      rotateInterval: "0",
     },
   ],
 }
